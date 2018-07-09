beffda348572d40804cef881623b4f8d1022c221
Devin Campbell <devin_m_campbell@yahoo.com>
2018-07-08 20:08:24 -0500

    Add files via upload

---
+++ setup.ps1
@@ -0,0 +1,18 @@
+SetExecutionPolicy AllSigned
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install FoxitReader
+choco install GoogleChrome
+choco install 7zip.install
+choco install vlc
+choco install putty.install
+choco install ccleaner
+choco install filezilla
+choco install teamviewer
+choco install malwarebytes
+choco install ChocolateyGUI
+choco install googledrive
+choco install classic-shell
+choco install crystaldiskinfo
+choco install qbittorrent
+choco install f.lux
+choco install pia
